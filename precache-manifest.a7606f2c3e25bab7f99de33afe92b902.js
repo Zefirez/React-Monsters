bd053e9d90b67c3c9c0fde29c0fbf05418279d15
Józek <przyjemniaczek@gmail.com>
2020-02-29 12:53:35 +0100

    Updates

---
+++ precache-manifest.a7606f2c3e25bab7f99de33afe92b902.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc75639ad61d7ff0056fcc28a8e2199",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d712eeb71624afa94279",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b49ede963a0f2ff5b4ef",
+    "url": "/static/js/2.62dce5ae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.62dce5ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d712eeb71624afa94279",
+    "url": "/static/js/main.03b2b192.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
